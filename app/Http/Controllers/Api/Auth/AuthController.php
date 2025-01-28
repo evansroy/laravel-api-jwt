c13570f5a35c73f821dff6663ed3cdfb87aa1aa7
Evans Roy <evansroysir@gmail.com>
2025-01-28 10:05:16 +0300

    Creat Auth Controller and Registration Request Rules

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /***
+     * Login Method
+     */
+
+     public function login()
+     {
+         //
+     }
+
+     /**
+      * Register Method
+      */
+
+      public function register()
+      {
+          //
+      }
+}
