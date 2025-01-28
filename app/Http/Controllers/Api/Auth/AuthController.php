a4de8992f8741084fe23e4e1105c0c3c7ca470c5
Evans Roy <evansroysir@gmail.com>
2025-01-28 10:40:04 +0300

    Implement Registration Functionality

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegistrationRequest;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Js;
 
 class AuthController extends Controller
 {
@@ -20,8 +23,32 @@ class AuthController extends Controller
       * Register Method
       */
 
-      public function register()
+      public function register(RegistrationRequest $request)
       {
-          //
+          $user = User::create($request->validated());
+
+          if ($user) {
+             $token = auth()->login($user);
+             return $this->responseWithToken($token, $user);
+          } else {
+                return response()->json([
+                    'status' => 'failed',
+                    'massage' => 'An error occurred while trying to create user',
+                ], 500);
+          }
       }
+
+      /**
+       * Return JWT Access Token
+       */
+
+       public function responseWithToken($token, $user)
+       {
+            return response()->json([
+                'status' => 'success',
+                'user' => $user,
+                'access_token' => $token,
+                'type' => 'bearer',
+            ]);
+       }
 }
