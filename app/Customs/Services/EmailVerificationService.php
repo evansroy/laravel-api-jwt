b9961bf2d4a2d06d47f693d7d4368d2b15bc523c
Evans Roy <evansroysir@gmail.com>
2025-01-28 14:40:04 +0300

    Implement Email Verification Functionality

--- app/Customs/Services/EmailVerificationService.php
+++ app/Customs/Services/EmailVerificationService.php
@@ -6,13 +6,82 @@ use App\Models\EmailVerificationToken;
 use App\Notifications\EmailVerificationNotification;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Str;
+use App\Models\User;
+
 class EmailVerificationService
 {
     /**
      * Send verification link to the user
      */
-    public function sendVerificationLink(object $user){
+    public function sendVerificationLink(object $user)
+    {
         Notification::send($user, new EmailVerificationNotification($this->generateVerificationLink($user->email)));
+    }
+
+    /**
+     * Check if user has Already verified email
+     */
+    public function checkIfEmailIsVerified(object $user) {
+        if ($user->email_verified_at) {
+           response()->json([
+               'status' => 'failed',
+               'message' => 'Email already verified'
+           ])->send();
+           exit;
+        }
+    }
+
+    /**
+     * Verify user email
+     */
+    public function verifyEmail(string $token, string $email) {
+        $user = User::where('email', $email)->first();
+        if (!$user) {
+            response()->json([
+                'status' => 'failed',
+                'message' => 'User not found'
+            ])->send();
+            exit;
+        }
+        $this->checkIfEmailIsVerified($user); 
+       $verifiedToken = $this->verifyToken($token, $email);
+       if($user->markEmailAsVerified()) {
+           $verifiedToken->delete();
+           return response()->json([
+               'status' => 'success',
+               'message' => 'Email verified successfully'
+           ]);
+       } else{
+           return response()->json([
+               'status' => 'failed',
+               'message' => 'Email verification failed, Please try againlater'
+           ]);
+       }
+       
+    }
+    /**
+     * Verify Token
+     */
+    public function verifyToken(string $token, string $email)
+    {
+        $token = EmailVerificationToken::where('token', $token)->where('email', $email)->first();
+        if ($token) {
+           if($token->expires_at < now()) {
+               return $token;
+           } else {
+               response()->json([
+                   'status' => 'failed',
+                   'message' => 'Token expired'
+               ])->send();
+               exit;
+           }
+        } else {
+            response()->json([
+                'status' => 'failed',
+                'message' => 'Invalid token'
+            ], 400)->send();
+            exit;
+        }
     }
     /**
      * 
--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,66 +5,73 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegistrationRequest;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\VerifyEmailRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Js;
 use App\Customs\Services\EmailVerificationService;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class AuthController extends Controller
 {
-    public function __construct(private EmailVerificationService $service)
-    {
-        
-    }
+    public function __construct(private EmailVerificationService $service) {}
     /***
      * Login Method
      */
 
-     public function login(LoginRequest $request)
-     {
-         $token = auth()->attempt($request->validated());
+    public function login(LoginRequest $request)
+    {
+        $token = auth()->attempt($request->validated());
 
-         if ($token) {
-             return $this->responseWithToken($token, auth()->user());
-         } else {
-             return response()->json([
-                 'status' => 'failed',
-                 'massage' => 'Invalid credentials',
-             ], 401);
-         }
-     }
-
-     /**
-      * Register Method
-      */
-
-      public function register(RegistrationRequest $request)
-      {
-          $user = User::create($request->validated());
-
-          if ($user) {
-            $this->service->sendVerificationLink($user);
-             $token = auth()->login($user);
-             return $this->responseWithToken($token, $user);
-          } else {
-                return response()->json([
-                    'status' => 'failed',
-                    'massage' => 'An error occurred while trying to create user',
-                ], 500);
-          }
-      }
-
-      /**
-       * Return JWT Access Token
-       */
-
-       public function responseWithToken($token, $user)
-       {
+        if ($token) {
+            return $this->responseWithToken($token, auth()->user());
+        } else {
             return response()->json([
-                'status' => 'success',
-                'user' => $user,
-                'access_token' => $token,
-                'type' => 'bearer',
-            ]);
-       }
+                'status' => 'failed',
+                'massage' => 'Invalid credentials',
+            ], 401);
+        }
+    }
+
+    /**
+     * Verify User Email
+     */
+
+    public function verifyUserEmail(VerifyEmailRequest $request)
+    {
+        return $this->service->verifyEmail($request->token, $request->email);
+    }
+    /**
+     * Register Method
+     */
+
+    public function register(RegistrationRequest $request)
+    {
+        $user = User::create($request->validated());
+
+        if ($user) {
+            $this->service->sendVerificationLink($user);
+            $token = auth()->login($user);
+            return $this->responseWithToken($token, $user);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'massage' => 'An error occurred while trying to create user',
+            ], 500);
+        }
+    }
+
+    /**
+     * Return JWT Access Token
+     */
+
+    public function responseWithToken($token, $user)
+    {
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'access_token' => $token,
+            'type' => 'bearer',
+        ]);
+    }
 }
